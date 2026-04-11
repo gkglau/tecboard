ce99d7e10201be4f736b4babba6cc824591d71f2
Glaucia Grossi <glau.kobashigawa.jp@gmail.com>
2026-04-11 23:24:19 +0900

    banner

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,15 +1,59 @@
 import "./App.css";
+import { Banner } from "./components/Banner";
 import { EventForm } from "./components/EventForm";
+import { Theme } from "./components/Theme";
 function App() {
+  const themes = [
+    {
+      id: 1,
+      name: "frontend",
+    },
+    {
+      id: 2,
+      name: "backend",
+    },
+    {
+      id: 3,
+      name: "devops",
+    },
+    {
+      id: 4,
+      name: "inteligencia artificial",
+    },
+    {
+      id: 5,
+      name: "data science",
+    },
+    {
+      id: 6,
+      name: "cloud",
+    },
+  ];
   return (
     <main>
       <header>
         <img src="/logo.png" />
       </header>
-      <section>
-        <img src="/hero-img.png" />
-      </section>
+      <Banner />
       <EventForm />
+      <section>
+        <Theme theme={themes[0]}></Theme>
+      </section>
+      <section>
+        <Theme theme={themes[1]}></Theme>
+      </section>
+      <section>
+        <Theme theme={themes[2]}></Theme>
+      </section>
+      <section>
+        <Theme theme={themes[3]}></Theme>
+      </section>
+      <section>
+        <Theme theme={themes[4]}></Theme>
+      </section>
+      <section>
+        <Theme theme={themes[5]}></Theme>
+      </section>
     </main>
   );
 }
---
+++ @latest/src/components/Banner/index.jsx
@@ -0,0 +1,8 @@
+import "./banner.styles.css";
+export function Banner() {
+  return (
+    <section className="banner">
+      <img src="/hero-img.png" />
+    </section>
+  );
+}
